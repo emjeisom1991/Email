942952b5bc9abc7175913f2aa45925310e9493e4
delete <emjeisom@ucl.br>
2022-03-14 21:28:55 -0300

    Enviando e-mails com Node 2

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 
-export const contato = (req: Request, res: Response) => {
+export const contato = async (req: Request, res: Response) => {
     // Passo 1: Configurar o transporter
     let transport = nodemailer.createTransport({
         host: "smtp.mailtrap.io",
@@ -13,10 +13,19 @@ export const contato = (req: Request, res: Response) => {
       });
 
     // Passo 2: Configurar a mensagem
+    let message ={
+        from: 'nao-responda@b7web.com.br',
+        to: 'Jefferson <jefferson@ig.com.br>',
+        replyTo: req.body.from,
+        subject: req.body.subject,
+        html: req.body.email,
+        text: req.body.email
+    }  
+
     // Passo 3: Enviar a mensagem
+    let info = await transport.sendMail(message);
 
+    console.log("INFO", info);
 
-
-
-    res.json({pong: true});
+    res.json({success: true});
 }
